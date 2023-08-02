42ed7950d9906307c13b3e61b7eb248f034f15fb
CubeSuite <cubesuite@outlook.com>
2023-08-02 19:35:22 +0100

    Initial commit

---
+++ StackFilterInserters/Patches/InserterInstancePatch.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace StackFilterInserters.Patches
+{
+    public class InserterDefinitionPatch 
+    {
+        private static bool stackInserterUnlocked = false;
+
+        [HarmonyPatch(typeof(InserterDefinition), "InitInstance")]
+        [HarmonyPostfix]
+        private static void setToStack(ref InserterInstance newInstance) {
+            if (newInstance.isFilter) {
+                newInstance.isStack = true;
+            }
+        }
+    }
+
+    //public class InserterInstancePatch
+    //{
+    //    private static bool stackInserterUnlocked = false;
+    //    private static bool hasPrinted = false;
+
+    //    [HarmonyPatch(typeof(InserterInstance), "Give")]
+    //    [HarmonyPrefix]
+    //    private static void setIsStack(InserterInstance __instance) {
+    //        if(__instance.isStack && __instance.isFilter) {
+    //            Debug.Log("Stack filter is giving");
+    //        }
+    //    }
+
+    //    [HarmonyPatch(typeof(InserterInstance), "Take")]
+    //    [HarmonyPrefix]
+    //    private static bool debugTake(InserterInstance __instance) {
+            
+
+    //        if (stackInserterUnlocked && __instance.isFilter && !__instance.isStack) {
+    //            __instance.isStack = true;
+    //            Debug.Log($"Setting filter inserter to stack mode");
+    //        }
+
+    //        if (__instance.isStack && __instance.isFilter) {
+    //            Debug.Log($"Take called for insterter: filter {__instance.isFilter}, {__instance.isStack}");
+    //        }
+
+    //        int NumHandSlotsRemaining = (__instance.isStack ? __instance.maxStackSize : 1) - __instance.currentHeldStackCount; // Amount left in hand
+    //        int resID_FilterID = (__instance.currentHeldResType > 0) ? __instance.currentHeldResType : __instance.filterType; // -1 if empty
+
+    //        if(__instance.isStack && __instance.isFilter) {
+    //            Debug.Log($"{NumHandSlotsRemaining} / {__instance.maxStackSize} hand slots available");
+    //            Debug.Log($"currentHelpResType: {__instance.currentHeldResType.ToString()}");
+    //            Debug.Log($"FilterType: {__instance.filterType}");
+    //            hasPrinted = true;
+    //        }
+
+    //        ResourceStack resourceStack;
+    //        if (__instance.takeResourceContainer.TakeAvailableRes(out resourceStack, resID_FilterID, NumHandSlotsRemaining)) {
+    //            __instance.resourceTakenThisFrame = resourceStack.id;
+    //            __instance.SetHeldItem(resourceStack.info);
+    //            Debug.Log($"Stack in hand: {resourceStack.info.displayName}, Count: {resourceStack.count}");
+    //            bool isHandFull = resourceStack.count >= NumHandSlotsRemaining;
+    //            Debug.Log(isHandFull ? "Hand is full" : "Hand is not full");
+    //            __instance.currentHeldStackCount += resourceStack.count;
+    //            Debug.Log($"New hand stack count: {__instance.currentHeldStackCount}");
+    //            if (isHandFull) {
+    //                Debug.Log($"Moving to give");
+    //                __instance.armState = InserterInstance.ArmState.SwingingToGive;
+    //                __instance.animState = 0f;
+    //                __instance.queuedSound = InserterInstance.InserterSound.Retract;
+    //            }
+    //        }
+
+    //        return false;
+    //    }
+    //}
+}
---
+++ StackFilterInserters/StackFilterInsertersPlugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using StackFilterInserters.Patches;
+using UnityEngine;
+
+namespace StackFilterInserters
+{
+    // TODO Review this file and update to your own requirements.
+
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class StackFilterInsertersPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.StackFilterInserters";
+        private const string PluginName = "StackFilterInserters";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+
+            Harmony.CreateAndPatchAll(typeof(InserterDefinitionPatch));
+        }
+    }
+}
